80a2f43d80d6a9c99b5c9e34fa4da9e21d0b69d6
David Packles <davidpackles@gmail.com>
2013-01-02 09:30:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SparkApp::Application.config.secret_token = '26f547112b76cbe082fa6a6b63e95370e28f431c1f606341d70b94c538815ea2caa8ff755a146a04f8afa36345de07ad0a9c8c0b20f459c06ebb12561f0c2b74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SparkApp::Application.config.session_store :cookie_store, key: '_spark_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SparkApp::Application.config.session_store :active_record_store
